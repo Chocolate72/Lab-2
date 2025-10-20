2a94ed1d5230e84f93fd56d8ad8b738027e569ef
Anuj Basnet <anujbasnet14@gmail.com>
2025-10-19 22:57:32 -0400

    Wrote codes to get references

---
+++ js/page-color-changer.js
@@ -0,0 +1,10 @@
+//to ensure the DOM is fully loaded before running the script
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Get references to the sliders and the html element
+  const redSlider = document.getElementById('red');
+  const greenSlider = document.getElementById('green');
+  const blueSlider = document.getElementById('blue');
+  const htmlElement = document.querySelector('html');
+
+});
